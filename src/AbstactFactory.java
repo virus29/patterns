6f3be52640707af7403fb10d574ace0818155f3f
virus29 <imya1986>
2017-09-25 23:52:47 +0300

    commit001

---
+++ src/AbstactFactory.java
@@ -0,0 +1,29 @@
+/**
+ * Шаблон "Фабрика Фабрик"
+ */
+public class AbstactFactory {
+}
+//interface Car{
+//    void drive();
+//}
+//class Toyota implements Car {
+//    @Override
+//    public void drive() {
+//        System.out.println("drive Toyota");
+//    }
+//}
+//class Audi implements Car {
+//    @Override
+//    public void drive() {
+//        System.out.println("drive Audi");
+//    }
+//}
+//class FactoryPattern{
+//    public Car create(String typeOfCar){
+//        switch (typeOfCar) {
+//            case "Toyota" : return new Toyota();
+//            case "Audi" : return new Audi();
+//            default: return null;
+//        }
+//    }
+//}
