7d9ee4ef4be76418acdd07445ea40ad0b03cd2f4
virus29 <imya1986>
2017-09-29 00:17:30 +0300

    Добавил шаблон Фабрика Фабрик. В одном проекте не может быть дублежей классов, поэтому первый нужно комментировать, чтобы работал второй

--- src/AbstractFactory/AbstactFactory.java
+++ src/AbstractFactory/AbstactFactory.java
@@ -1,16 +1,18 @@
+package AbstractFactory;
+
 /**
  * Шаблон "Фабрика Фабрик"
  */
 public class AbstactFactory {
     public static void main(String[] args) {
-        Factory carFactory = new AbstractFactoryPattern().createFactory("Car");
-        Factory tankFactory = new AbstractFactoryPattern().createFactory("Tank");
-        Car toyota = carFactory.createCar("Toyota");
-        Car audi = carFactory.createCar("Audi");
+        Factory carFactory = new AbstractFactoryPattern().createFactory("AbstractFactory.Car");
+        Factory tankFactory = new AbstractFactoryPattern().createFactory("AbstractFactory.Tank");
+        Car toyota = carFactory.createCar("AbstractFactory.Toyota");
+        Car audi = carFactory.createCar("AbstractFactory.Audi");
         toyota.drive();
         audi.drive();
-        Tank t51 = tankFactory.createTank("T51");
-        Tank t52 = tankFactory.createTank("T52");
+        Tank t51 = tankFactory.createTank("AbstractFactory.T51");
+        Tank t52 = tankFactory.createTank("AbstractFactory.T52");
         t51.drive();
         t52.drive();
     }
@@ -21,20 +23,20 @@ interface Car {
 class Toyota implements Car {
     @Override
     public void drive() {
-        System.out.println("drive Toyota");
+        System.out.println("drive AbstractFactory.Toyota");
     }
 }
 class Audi implements Car {
     @Override
     public void drive() {
-        System.out.println("drive Audi");
+        System.out.println("drive AbstractFactory.Audi");
     }
 }
 class CarFactory implements Factory{
     public Car createCar(String typeOfCar){
         switch (typeOfCar) {
-            case "Toyota" : return new Toyota();
-            case "Audi" : return new Audi();
+            case "AbstractFactory.Toyota" : return new Toyota();
+            case "AbstractFactory.Audi" : return new Audi();
             default: return null;
         }
     }
@@ -50,20 +52,20 @@ interface Tank{
 class T51 implements Tank {
     @Override
     public void drive() {
-        System.out.println("drive T51");
+        System.out.println("drive AbstractFactory.T51");
     }
 }
 class T52 implements Tank {
     @Override
     public void drive() {
-        System.out.println("drive T52");
+        System.out.println("drive AbstractFactory.T52");
     }
 }
 class TankFactory implements Factory{
     public Tank createTank(String typeOfTank){
         switch (typeOfTank) {
-            case "T51" : return new T51();
-            case "T52" : return new T52();
+            case "AbstractFactory.T51" : return new T51();
+            case "AbstractFactory.T52" : return new T52();
             default: return null;
         }
     }
@@ -81,8 +83,8 @@ interface Factory {
 class AbstractFactoryPattern {
     Factory createFactory(String typeOfFactory) {
         switch (typeOfFactory) {
-            case "Car" : return new CarFactory();
-            case "Tank" : return new TankFactory();
+            case "AbstractFactory.Car" : return new CarFactory();
+            case "AbstractFactory.Tank" : return new TankFactory();
             default: return null;
         }
     }
---
+++ src/Factory/Factory.java
@@ -0,0 +1,35 @@
+package Factory;
+
+class Factory {
+    public static void main(String[] args) {
+        FactoryPattern factory = new FactoryPattern();
+        Car toyota = factory.create("Toyota");
+        Car audi = factory.create("Audi");
+        toyota.drive();
+        audi.drive();
+    }
+}
+interface Car{
+    void drive();
+}
+class Toyota implements Car {
+    @Override
+    public void drive() {
+        System.out.println("drive Toyota");
+    }
+}
+class Audi implements Car {
+    @Override
+    public void drive() {
+        System.out.println("drive Audi");
+    }
+}
+class FactoryPattern{
+    public Car create(String typeOfCar){
+        switch (typeOfCar) {
+            case "Toyota" : return new Toyota();
+            case "Audi" : return new Audi();
+            default: return null;
+        }
+    }
+}
